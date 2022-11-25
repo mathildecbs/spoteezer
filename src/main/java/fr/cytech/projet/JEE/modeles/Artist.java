ee5dcffea91c227b16cb0b9b203703fd543a5ea4
cytech <chambasmat@cy-tech.fr>
2022-11-25 16:16:30 +0100

    Artist modify modele file

--- src/main/java/fr/cytech/projet/JEE/modeles/Artist.java
+++ src/main/java/fr/cytech/projet/JEE/modeles/Artist.java
@@ -1,10 +1,16 @@
 package fr.cytech.projet.JEE.modeles;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
@@ -14,12 +20,23 @@ public class Artist {
 	@Id @GeneratedValue
 	private Long id;
 	
-	
 	@Column @NotNull
 	private String name;
 	
 	@Temporal(TemporalType.DATE)
 	private Date debutDate;
+
+	@ManyToMany
+	@JoinTable( name = "Artist_Album",
+    joinColumns = @JoinColumn( name = "artist_id" ),
+    inverseJoinColumns = @JoinColumn( name = "album_id" ) )
+	private List<Album> album = new ArrayList<>();
+	
+	@ManyToMany
+	@JoinTable( name = "Artist_Song",
+    joinColumns = @JoinColumn( name = "artist_id" ),
+    inverseJoinColumns = @JoinColumn( name = "song_id" ) )
+	private List<Song> Song = new ArrayList<>();
 
 	public Long getId() {
 		return id;
