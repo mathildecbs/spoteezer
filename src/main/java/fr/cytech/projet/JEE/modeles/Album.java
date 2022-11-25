c57838d821e310108957ef106567c53585670bc9
cytech <chambasmat@cy-tech.fr>
2022-11-25 15:03:30 +0100

    Correct bug for database

--- src/main/java/fr/cytech/projet/JEE/modeles/Album.java
+++ src/main/java/fr/cytech/projet/JEE/modeles/Album.java
@@ -4,12 +4,14 @@ import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
+@Entity(name="Album")
 public class Album {
 	@Id @GeneratedValue
 	private Long id;
