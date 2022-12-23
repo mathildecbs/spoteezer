29568f4ea72e90ec967043697e12b09d0e28796e
Mathilde <chambasmat@cy-tech.fr>
2022-12-23 19:10:03 +0100

    comment on song repo

--- src/main/java/fr/cytech/projet/JEE/repository/SongRepository.java
+++ src/main/java/fr/cytech/projet/JEE/repository/SongRepository.java
@@ -9,7 +9,12 @@ import fr.cytech.projet.JEE.modeles.Song;
 
 @Repository("songRepository")
 public interface SongRepository<T extends Song> extends JpaRepository<Song, Long>  {
+	//Création d'une méthode pour récupérer une musique grâce à son nom
 	public Song findByName(String name);
+	
+	//Création d'une méthode pour récupérer toutes les chansons avec le même nom
 	public List<Song> findAllByName(String name);
+	
+	//Création d'une méthode pour récupérer les chansons grace à l'id de l'album
 	public List<Song> findByAlbumId(Long AlbumId);
 }
