78e5f501d0ceb2aae7af30c69c4dd2fc31e451aa
Mathilde <chambasmat@cy-tech.fr>
2022-12-23 19:07:53 +0100

    comment on playlist repo

--- src/main/java/fr/cytech/projet/JEE/repository/PlaylistRepository.java
+++ src/main/java/fr/cytech/projet/JEE/repository/PlaylistRepository.java
@@ -9,6 +9,9 @@ import fr.cytech.projet.JEE.modeles.Playlist;
 
 @Repository("playlistRepository")
 public interface PlaylistRepository extends JpaRepository<Playlist,Long>{
+	//Création d'une méthode pour récupérer toutes les playlist d'un utilisateur grace à son id
 	public List<Playlist> findAllPlaylistByUserId(long userId);
+	
+	//Création d'une méthode pour récupérer une playlist grâce à son id
 	public Playlist findPlaylistById(long id);
 }
